1120c71f3a2b53f7924b3165e9168d0915b610f6
alexanderjlcote <alexanderjlcote@gmail.com>
2025-09-10 14:40:01 -0400

    Modified Self Files

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+while True:    
+    print("-------------------")
+
+    x = int(input())
+    y = input("")
+    z = int(input())
+
+    w = []
+    for i in range(int(z)):
+        w.append(x)
+        for a in w:
+            x * x
+    
+    # q = x * len(w)
+
+
+
+    # if y == "+":
+    #     print(f"= {x + z}")
+    # elif y == "-":
+    #     print(f"= {x - z}")
+    # elif y == "*":
+    #     print(f"= {x * z}")
+    # elif y == "/":
+    #     print(f"= {x / z}")
+    
+    if y == "^":
+        # for a in w:
+        #     x * x
+        print(f"{x ** y}")
